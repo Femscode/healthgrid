842fa0ebe03261e4fb965e3c2cc9fc548bbc8879
HealthGrid Team <healthgrid@example.com>
2025-09-12 00:03:18 +0100

    wraknew

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import build from '@hono/vite-build/cloudflare-pages'
-import devServer from '@hono/vite-dev-server'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import honoViteBuild from "@hono/vite-build";
 
 export default defineConfig({
   plugins: [
-    build(),
-    devServer({
-      adapter,
-      entry: 'src/index.tsx'
-    })
-  ]
-})
+    honoViteBuild({
+      entry: "src/index.tsx",
+     }),
+  ],
+  build: {
+    target: "esnext",
+    outDir: "dist",
+    rollupOptions: {
+      input: "src/index.tsx",
+    },
+  },
+});
